1d2b15aa70cb69e00d57bce1328bc0db6f83c472
derrick056 <derrickgatete056@gmail.com>
2024-12-14 14:59:07 +0200

    Arithmetic Exercise

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,11 @@
+#assignment of values to their valuable names
+number1 = int(10) 
+number2 = int(5) 
+
+sum = number1 + number2
+difference = number2 - number1
+product = number1 * number2
+
+print("Addition of 10 and 5 is", sum)
+print("Subtraction of 10 and 5 is", difference)
+print("Multiplication of 10 and 5 is", product)
