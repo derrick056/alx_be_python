30f104f6d858d44a80ee70060131baa56c9a1c16
derrick056 <derrickgatete056@gmail.com>
2024-12-14 16:54:42 +0200

    Simple Interest Calculator

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,8 @@
+#assignment of values to their valuable names
+principal = 1000
+rate = float(0.05)
+time = 3
+
+interest = float(principal * rate * time)
+
+print("The simple interest is", interest)
