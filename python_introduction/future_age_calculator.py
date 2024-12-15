337fb053ac1d6cee15bea5574ceeaed4d9615b44
derrick056 <derrickgatete056@gmail.com>
2024-12-15 14:34:41 +0200

    correction

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,11 @@
+#assingning of valiables
+c_year = 2023
+t_year = 2050
+
+age_target = int(t_year - c_year)
+
+user_age = int(input("How old are you?"))
+
+f_age = age_target + user_age
+
+print("In 2050, you will be", f_age,"years old")
