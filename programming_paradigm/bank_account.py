74b2a734a30fc0e144a083ea12566f70714e9ef1
derrick056 <derrickgatete056@gmail.com>
2025-01-25 15:27:25 +0200

    hoping to work

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -19,4 +19,4 @@ class BankAccount:
       
            
     def display_balance(self:float):
-        print(f"Current Balance: ${self.account_balance}")
+        print(f"Current Balance: ${self.account_balance:.2f}")
