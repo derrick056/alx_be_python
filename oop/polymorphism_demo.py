26812a98fa6af0641b97a11744effdc63235b13c
derrick056 <derrickgatete056@gmail.com>
2025-02-02 14:20:03 +0200

    overiding

---
+++ oop/polymorphism_demo.py
@@ -0,0 +1,24 @@
+import math
+
+class Shape:
+    def area(self):
+        raise NotImplementedError ("classes need to override this method")
+    
+class Rectangle(Shape):
+    def __init__(self, length, width):
+        super().__init__()
+
+        self.length= length
+        self.width= width
+
+    def area(self):
+        return self.length * self. width
+    
+class Circle(Shape):
+    def __init__(self, radius):
+        """Initialize the circle with a radius."""
+        self.radius = radius
+    
+    def area(self):
+        """Calculate and return the area of the circle."""
+        return math.pi * (self.radius ** 2)
